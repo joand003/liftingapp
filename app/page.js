d670272d6a76efc420f97b112e27ea3568e2b2d4
Joshua Andersland <joshua.andersland@gmail.com>
2023-11-13 10:51:31 -0600

    changed env varible of nextauth url which was likely login problem

--- app/page.js
+++ app/page.js
@@ -5,7 +5,7 @@ import { useSession } from 'next-auth/react'
 
 
 export default function Home() {
-  const { data: session, status } = useSession();
+  const { data: session } = useSession();
 
   return (
     <div className="flex min-h-screen flex-col items-center px-4 md:px-12 lg:px-24">
